4f65de184d64d6b043fd9e00653e22e7063f9594
Roque Santos <roque-100@hotmail.com>
2024-11-16 16:55:37 -0300

    added recipe and loot table for roaster and cooking recipe for coffee beans

--- src/main/java/com/udison999/coffeecraft/item/ModCreativeModeTabs.java
+++ src/main/java/com/udison999/coffeecraft/item/ModCreativeModeTabs.java
@@ -5,8 +5,6 @@ import com.udison999.coffeecraft.block.ModBlocks;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredRegister;
